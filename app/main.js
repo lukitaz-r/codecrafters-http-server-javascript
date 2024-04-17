1724a20b7661a6149e9e02b0f3d4bae12f60dd5b
Lukita <lukitaz.ramirezjajaja@gmail.com>
2024-04-16 23:04:52 -0300

    pass 3st stage

--- app/main.js
+++ app/main.js
@@ -4,13 +4,17 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.end("HTTP/1.1 200 OK\r\n\r\n");
-  });
-  socket.on("close", () => {
-    socket.end()
-    server.close()
-  });
+    socket.on("data", (data) => {
+        let request = data.toString().split("\r\n")
+        let path = request[0].split(" ")
+        if(path[1] === "/") socket.write('HTTP/1.1 200 OK\r\n\r\n')
+        else socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+        socket.end()
+    });
+    socket.on("close", () => {
+        socket.end()
+        server.close()
+    });
 });
 
 server.listen(4221, "localhost");
