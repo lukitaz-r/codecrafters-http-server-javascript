b781abeaba9850d4022a8008b2ab8acf9cb03ce1
Lukita <lukitaz.ramirezjajaja@gmail.com>
2024-04-16 21:51:49 -0300

    pass 2st stage

--- app/main.js
+++ app/main.js
@@ -4,9 +4,12 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
+  socket.on("data", () => {
+    socket.end("HTTP/1.1 200 OK\r\n\r\n");
+  });
   socket.on("close", () => {
-    socket.end();
-    server.close();
+    socket.end()
+    server.close()
   });
 });
 
