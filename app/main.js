90b47844cb864499497365732e9e21d5519917c0
Lukita <lukitaz.ramirezjajaja@gmail.com>
2024-04-16 23:32:32 -0300

    pass 5st stage

--- app/main.js
+++ app/main.js
@@ -7,21 +7,24 @@ const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         let request = data.toString().split("\r\n");
         let path = request[0].split(" ");
-        if (path[1] === "/") {
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        if (path[1] === "/") socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        else if (path[1].includes("/echo/")) {
+            let content = path[1].replace("/echo/", "");
+            socket.write(
+                `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+            );
         } else if (path[1].includes("/user-agent")) {
             let userAgent = "";
-            for (let i = 1; i < request.length(); i++) {
-                if (request[i].includes("User-Agent:")) {
+            for (let i = 1; i < request.length; i++) {
+                if (request[i].startsWith("User-Agent:")) {
                     userAgent = request[i].substring(12);
+                    break;
                 }
             }
-            socket.write(
-                `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
-            );
-        } else {
-            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        }
+            const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+            1;
+            socket.write(response);
+        } else socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         socket.end();
     });
     socket.on("close", () => {
